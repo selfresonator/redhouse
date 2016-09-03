22720fc8ef494213cf2217cb7d85746c9af9357f
selfresonator <selfresonator@gmail.com>
2016-09-02 19:13:21 -0700

    Includes estTimeout for testing

--- client/src/app.js
+++ client/src/app.js
@@ -4,6 +4,9 @@ var app;
   app = {
     init: function() {
         $('body').append('<div>Test Text</div>');
+        setTimeout(function() {
+          $('body').append('<div>Test Text</div>');
+        }, 3000);
     }
   };
 })();
